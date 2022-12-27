604ff961de658ec6253158456996a260a035c3bc
Alexei <alexei@alexey.com>
2022-12-27 08:52:20 +0300

    two methods

--- Program.cs
+++ Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] Array(int N)
+{
+    string[] newArray = new string[N];
+    for (int i = 0; i < N; i++)
+    {
+        newArray[i] = Console.ReadLine();
+    }
+    return newArray;
+}
+
+void ShowArray(string[] Array)
+{
+    Console.Write("[");
+    for (int i = 0; i < Array.Length - 1; i++)
+    {
+        Console.Write($"'{Array[i]}', ");
+    }
+    int c = Array.Length - 1;
+    Console.Write($"'{Array[c]}']");
+    Console.WriteLine();
+}
+
+int Size(string[] Array)
+{
+    int M = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        int b = Array[i].Length;
+        if (b <= 3) M++;
+    }
+    return M;
+}
+
+string[] ToThree(int M, string[] Array)
+{
+    string[] newArray = new string[M];
+    int j = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        int b = Array[i].Length;
+        if (b <= 3)
+        {
+            newArray[j] = Array[i];
+            j++;
+        }
+
+    }
+    return newArray;
+}
+
+
+Console.WriteLine("Input size of array and it's elements:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string[] arr = Array(N);
+
+ShowArray(arr);
+
+string[] arr2 = ToThree(Size(arr), arr);
+
+ShowArray(arr2);
